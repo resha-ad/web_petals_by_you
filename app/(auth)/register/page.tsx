b2901c0c0ba7bd7e38e317ca3a8960af5dee0e4a
resha-ad <adhikariresha@gmail.com>
2025-12-26 16:06:25 +0545

    added register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../_components/RegisterForm";
+
+export default function Page() {
+    return (
+        <>
+            <h1 className="text-2xl text-pink-700 font-semibold text-center">
+                Create Your Account
+            </h1>
+            <RegisterForm />
+        </>
+    );
+}
