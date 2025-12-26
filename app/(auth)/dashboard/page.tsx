b230509ab863d516f777492b1819c178fbc24324
resha-ad <adhikariresha@gmail.com>
2025-12-26 16:07:55 +0545

    added dashboard page

---
+++ app/(auth)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl text-pink-700 font-semibold">
+                Dashboard
+            </h1>
+            <p>This is a dummy dashboard for Sprint 1.</p>
+        </div>
+    );
+}
