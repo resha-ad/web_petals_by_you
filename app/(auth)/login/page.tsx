5b6d0462b41b40666e4f114991cff55caa148d57
resha-ad <adhikariresha@gmail.com>
2025-12-26 16:03:23 +0545

    added login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../_components/LoginForm";
+
+export default function Page() {
+    return (
+        <>
+            <h1 className="text-2xl text-pink-700 font-semibold text-center">
+                Login to Petals By You
+            </h1>
+            <LoginForm />
+        </>
+    );
+}
